6f939bb26de68455e7d62384f14dfc54860355b0
Hashim2009 <94174587+Hashim2009@users.noreply.github.com>
2022-10-26 20:40:51 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+function send2x2() {
+    window.location = "2x2.html";
+}
+
+function send3x3() {
+    window.location = "3x3.html";
+}
+
+function send4x4() {
+    window.location = "4x4.html";
+}
+
+function send5x5() {
+    window.location = "5x5.html";
+}
+
+function send6x6() {
+    window.location = "6x6.html";
+}
+
+function send7x7() {
+    window.location = "7x7.html";
+}
+
+function sendpyraminx() {
+    window.location = "pyraminx.html";
+}
+
+function sendskewb() {
+    window.location = "skewb.html";
+}
+
+function sendclock() {
+    window.location = "clock.html";
+}
+
+function sendsquare1() {
+    window.location = "square_1.html";
+}
+
+function sendmega() {
+    window.location = "megaminx.html"
+}
+
+function exit() {
+    window.location = "index.html";
+}
